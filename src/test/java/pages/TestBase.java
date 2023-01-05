bb09241678dcc76ef70c599ecbe77494ff7b8d23
Gulnara <gulnara0706@yandex.ru>
2023-01-05 12:18:58 +0300

    ++

--- src/test/java/pages/TestBase.java
+++ src/test/java/pages/TestBase.java
@@ -26,14 +26,6 @@ public class TestBase {
         Configuration.baseUrl = "https://www.speechpro.ru/";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-
-
-
-
-
-
-
     }
         @AfterEach
         void addAttachments () {
